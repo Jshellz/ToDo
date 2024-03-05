e9e9031685c5e5bdf9b4a958b2f58aef1a9744bd
Jshellz <jshells97@yandex.com>
2024-03-05 17:47:14 +0300

    fix_model_controller_db_and_create_funcs_create_delete_findAll

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"ToDoOnGo/controller"
 	"ToDoOnGo/db_app"
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 var host = "localhost:8000"
-var panicRunHost = "failed to run"
 
 func init() {
 	db_app.ConnectToDB()
@@ -18,8 +18,9 @@ func main() {
 	r := gin.Default()
 	r.GET("/", controller.FindAllToDo)
 	r.POST("/createToDo", controller.CreateToDo)
+	r.DELETE("/deleteToDo/:id", controller.DeleteToDo)
 
 	if err := r.Run(host); err != nil {
-		panic(panicRunHost)
+		fmt.Println("Ok")
 	}
 }
